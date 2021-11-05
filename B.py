5b00d6ecb5e36a0e0ff67e5a7bb4836361c2f5ed
ilya1200 <ilya1200@gmail.com>
2021-11-05 15:19:39 +0200

    Add exercise B

---
+++ B.py
@@ -0,0 +1,7 @@
+number_of_iterations = 5
+
+first_iteration = 1
+last_iteration = first_iteration + number_of_iterations-1
+
+for i in range(first_iteration, last_iteration+1):
+    print("Iteration number %d" % i)
