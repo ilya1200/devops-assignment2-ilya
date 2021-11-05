77e30c46547adc0f3b4366042e7b7a80af5a50ef
ilya1200 <ilya1200@gmail.com>
2021-11-05 16:36:23 +0200

    Add exercise E

---
+++ E.py
@@ -0,0 +1,13 @@
+age = 20
+last_name_first_letter = "L"
+shekels_dollar_currency = 3.12
+been_abroad = True
+apartment_number = 13
+
+print(f"Age:{age}")
+print(f"First letter of last name:{last_name_first_letter}")
+print(f"shekels-dollar currency:{shekels_dollar_currency:.2f}")
+print(f"Did you fly abroad:{been_abroad}")
+print(f"apartment_number:{apartment_number}")
+
+print(age + shekels_dollar_currency)
