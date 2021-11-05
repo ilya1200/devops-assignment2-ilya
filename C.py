25ba5d4ee96f8ac933dfd998a5c7bdf2ee519a7c
ilya1200 <ilya1200@gmail.com>
2021-11-05 15:28:43 +0200

    Add exercise C

---
+++ C.py
@@ -0,0 +1,13 @@
+season_number = 1
+season_name = f"Season name not found for season number {season_number}"
+
+if season_number == 1:
+    season_name = "summer"
+elif season_number == 2:
+    season_name = "winter"
+elif season_number == 3:
+    season_name = "fall"
+elif season_number == 4:
+    season_name = "spring"
+
+print(season_name)
