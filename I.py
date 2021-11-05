a1d39bbed54ba80dc891a212676031356cec5201
ilya1200 <ilya1200@gmail.com>
2021-11-05 16:50:39 +0200

    Add exercise I

---
+++ I.py
@@ -0,0 +1,13 @@
+def add(first_number, second_number):
+    return first_number + second_number
+
+
+def concat_with_space(str1, str2):
+    return str1 + " " + str2
+
+
+sum1 = add(17, 89)
+print(sum1)
+
+new_str = concat_with_space("AB", "CD")
+print(new_str)
